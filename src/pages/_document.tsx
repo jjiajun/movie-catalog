dae7922465590444b00e4e4ea912a5a56d948af1
Tristan Teo <90031266+jjiajun@users.noreply.github.com>
2023-01-21 16:15:50 +0800

    Remove placeholder page content and styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* eslint-disable react/jsx-filename-extension */
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+import React from 'react';
 
 export default function Document() {
   return (
@@ -9,5 +13,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
